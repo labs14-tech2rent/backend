de668ce7ab27eadb205f7979f8bc73b2e2f536bc
Doug <me@douglasbowen.com>
2019-07-10 14:21:17 -0400

    add initial server

---
+++ api/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+server.get("/test", (req, res) => {
+  let testData = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten"
+  ];
+  res.status(200).json(testData);
+});
+
+module.exports = server;

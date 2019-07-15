d6893a45e6e3c6f825b276738dd30ff9ab94e194
Lidii <lidiiany92@gmail.com>
2019-07-15 16:55:27 -0400

    moved passportjs variables to env file

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,12 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const passportKeys = require('./passportKeys');
+require('dotenv').config()
 
 passport.use(
     new GoogleStrategy({
         callbackURL: '/test',
-        clientID: passportKeys.google.clientID,
-        clientSecret: passportKeys.google.clientSecret
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
     },
     () => {})
 )
--- config/passportKeys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        clientID: "644910688403-idnej2nmvuop9has6g03c7iepg91kscv.apps.googleusercontent.com",
-        clientSecret: "I89ogrqkmEdfnumbzgGcZzky"
-    }
-}

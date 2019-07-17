6710f0d2595f4f48b0300e43c64db5e6a9bc58d8
Doug <me@douglasbowen.com>
2019-07-17 14:02:29 -0400

    test commit

--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const db = require("../data/dbConfig");
 
-
 const server = express();
 
 const helmet = require("helmet");
 const cors = require("cors");
+
+const jwt = require("express-jwt");
+const jwtAuthz = require("express-jwt-authz");
+const jwksRsa = require("jwks-rsa");
 
 server.use(helmet());
 server.use(express.json());
@@ -15,19 +18,30 @@ server.get("/", (req, res) => {
   res.status(200).json({ api: "up" });
 });
 
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://dev-gco3gwsp.auth0.com/.well-known/jwks.json`
+  }),
+
+  audience: "YOUR_API_IDENTIFIER",
+  issuer: `https://dev-gco3gwsp.auth0.com/`,
+  algorithms: ["RS256"]
+});
 
 //Routes
 
 //User
-const userRoutes = require('../routes/users/usersRouter');
-server.use('/api/users', userRoutes);
+const userRoutes = require("../routes/users/usersRouter");
+server.use("/api/users", userRoutes);
 
 //Auth
-const authRoutes = require('../routes/auth/authRouter');
-server.use('/api/auth', authRoutes);
+const authRoutes = require("../routes/auth/authRouter");
+server.use("/api/auth", authRoutes);
 
-
-server.get("/test", (req, res) => {
+server.get("/test", checkJwt, (req, res) => {
   let testData = [
     "one",
     "two",

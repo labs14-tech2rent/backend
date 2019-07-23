a550a452e92c430dbc706ef18fd63e6c97fe0018
Doug <me@douglasbowen.com>
2019-07-23 14:19:00 -0400

    change callback url

--- api/server.js
+++ api/server.js
@@ -22,8 +22,7 @@ const strategy = new Auth0Strategy(
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:
-      process.env.AUTH0_CALLBACK_URL || "http://localhost:5000/callback"
+    callbackURL: process.env.AUTH0_CALLBACK_URL
   },
   function(accessToken, refreshToken, extraParams, profile, done) {
     /*

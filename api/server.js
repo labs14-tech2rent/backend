61bf95e553692c8b3cee25fe7fe547b3d07e3ad6
Doug <me@douglasbowen.com>
2019-07-24 11:44:13 -0400

    change db variable in server.js

--- api/server.js
+++ api/server.js
@@ -1,44 +1,44 @@
-const express = require('express');
+const express = require("express");
 // const passportSetup = require('../config/passport-setup');
 
 const server = express();
 
-const helmet = require('helmet');
-const cors = require('cors');
-const db = require('../data/dbConfig');
+const helmet = require("helmet");
+const cors = require("cors");
+const dba = require("../data/dbConfig");
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.status(200).json({ api: 'up' });
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
 });
 
 // Routes
 
 // User
-const userRoutes = require('../routes/users/usersRouter');
+const userRoutes = require("../routes/users/usersRouter");
 
-server.use('/api/users', userRoutes);
+server.use("/api/users", userRoutes);
 
 // Auth
-const authRoutes = require('../routes/auth/authRouter');
+const authRoutes = require("../routes/auth/authRouter");
 
-server.use('/api/auth', authRoutes);
+server.use("/api/auth", authRoutes);
 
-server.get('/test', (req, res) => {
+server.get("/test", (req, res) => {
   const testData = [
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine',
-    'ten',
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten"
   ];
   res.status(200).json(testData);
 });

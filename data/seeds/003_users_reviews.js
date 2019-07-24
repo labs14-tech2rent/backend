9ee7c6d43d5d1e618dc2346e4192aa84b49d370a
Lidii <lidiiany92@gmail.com>
2019-07-24 18:15:05 -0400

    Fixed user reviews table

--- data/seeds/003_users_reviews.js
+++ data/seeds/003_users_reviews.js
@@ -1,13 +1,49 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users_reviews')
+    .truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users_reviews').insert([
+        {
+          user_id: 1,
+          reviewer_id: 2,
+          rating: 4.5,
+          review_title: "Great Job",
+          review_body: "Love this user so much! Excellent work and great service. Always rent things from him"
+
+        },
+        {
+          user_id: 1,
+          reviewer_id: 3,
+          rating: 4.0,
+          review_title: "Very Good",
+          review_body: "Love this user so much! Excellent work and great service. Always rent things from him"
+        },
+        {
+          user_id: 1,
+          reviewer_id: 3,
+          rating: 5.0,
+          review_title: "Amazing",
+          review_body: "Love this user so much! Excellent work and great service. Always rent things from him"
+        },
+
+        {
+          user_id: 2,
+          reviewer_id: 3,
+          rating: 5.0,
+          review_title: "Amazing",
+          review_body: "Love this user so much! Excellent work and great service. Always rent things from him"
+        },
+
+        {
+          user_id: 2,
+          reviewer_id: 3,
+          rating: 5.0,
+          review_title: "Amazing",
+          review_body: "Love this user so much! Excellent work and great service. Always rent things from him"
+        }
       ]);
     });
 };

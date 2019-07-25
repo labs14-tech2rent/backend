286342df2fbc306fa2e8c963620e76119ad7a973
Doug <me@douglasbowen.com>
2019-07-25 13:34:21 -0400

    migrations changes

--- data/migrations1/20190710163028_users.js
+++ data/migrations1/20190710163028_users.js
@@ -1,33 +1,36 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('items', tbl => {
+  return knex.schema.createTable('users', tbl => {
     tbl.increments();
 
     tbl
-      .integer('users_ownerId', 255)
-      .references('id')
-      .inTable('users')
-      .notNull();
+      .string('auth0_user_id', 255)
+      .notNullable()
+      .unique();
 
-    tbl.string('name', 255).notNull();
+    tbl.string('email', 255).notNullable();
 
-    tbl.decimal('price', 8, 2).notNull();
+    tbl.string('name', 128).notNullable();
 
-    tbl.string('picture', 255);
+    tbl.string('profile_picture', 255);
 
-    tbl.string('location', 255);
+    tbl.string('phone', 255);
 
-    tbl.string('category', 255);
+    tbl.string('date_of_birth', 255);
 
-    tbl.string('description', 255);
+    tbl.string('preferred_payment_type', 255);
 
-    tbl.boolean('available', 255);
+    tbl.string('street', 255);
 
-    tbl.string('payment_type', 255);
+    tbl.string('city', 255);
 
-    tbl.decimal('avarage_raiting', 8, 2);
+    tbl.string('state', 255);
+
+    tbl.integer('zip_code', 6);
+
+    tbl.decimal('average_rating', 8, 2);
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('items');
+  return knex.schema.dropTableIfExists('users');
 };

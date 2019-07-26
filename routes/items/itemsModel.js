b45f03bb13158564b0f8d30e6f58af54783bba7d
Lidii <lidiiany92@gmail.com>
2019-07-26 13:57:50 -0400

    Added put endpoints for items

--- routes/items/itemsModel.js
+++ routes/items/itemsModel.js
@@ -3,7 +3,8 @@ const db = require('../../data/dbConfig');
 
 module.exports = {
     getAll,
-    getItemById
+    getItemById,
+    updateItem
 }
 
 function getAll() {
@@ -12,4 +13,9 @@ function getAll() {
 
 function getItemById(id) {
     return db('items').where({id}).first();
+}
+
+async function updateItem(id, updatedItem) {
+    const updated = await getItemById(id).update(updatedItem);
+    return updated;
 }

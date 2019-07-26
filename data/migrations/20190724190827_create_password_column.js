3ecd980df2f42762b31eb9f349d4c34715ab28ae
Doug <me@douglasbowen.com>
2019-07-26 12:43:56 -0400

    rename migrations

--- data/migrations/20190724190827_create_password_column.js
+++
@@ -1,9 +0,0 @@
-exports.up = function(knex) {
-  return knex.schema.table('users', function(tbl) {
-    tbl.string('password', 128);
-  });
-};
-
-exports.down = function(knex) {
-  return knex.schema.dropColumn('password');
-};

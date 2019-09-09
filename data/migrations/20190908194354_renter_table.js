82cd796238237d1b92634c89143fbc2442af1511
Erik Sandoval <erik.sandoval@icloud.com>
2019-09-08 19:57:07 -0700

    creating renter_table

---
+++ data/migrations/20190908194354_renter_table.js
@@ -0,0 +1,22 @@
+exports.up = function(knex) {
+  return knex.schema.table('renter_table', function(tbl) {
+    tbl.increments();
+    tbl
+      .integer('item_id', 255)
+      .references('id')
+      .inTable('items')
+      .notNull();
+    tbl
+      .integer('renter_id', 255)
+      .references('id')
+      .inTable('users')
+      .notNull();
+    tbl.decimal('offer_amount', 8, 2).notNull();
+    tbl.date('date_from').notNull();
+    tbl.date('date_to').notNull();
+    tbl.boolean('rent_agree');
+    tbl.boolean('returned');
+  });
+};
+
+exports.down = function(knex) {};
